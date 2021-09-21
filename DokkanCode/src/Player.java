cd347768823166c4c41ad0d00e5e865d0fc77750
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2021-09-21 16:12:43 -0700

    Small updates
    These changes are quality updates

--- DokkanCode/src/Player.java
+++ DokkanCode/src/Player.java
@@ -3,4 +3,6 @@ import java.util.ArrayList;
 public class Player {
     ArrayList<Unit> myCharacterList = new ArrayList<>();
     ArrayList<Item> myMedals = new ArrayList<>();
+    //maybe make medals in a stack to store the amount of medals along with items
+    
 }
